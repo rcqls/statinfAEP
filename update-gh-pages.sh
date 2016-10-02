22539e5304f1944eb1b1bf8b0179e58ee0bf0518
R Cqls <rdrouilh@gmail.fr>
2016-10-02 13:41:25 +0200

    add comment to generate cqls.js

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,9 +3,11 @@
 #git checkout master aep/aep_html.dyn
 #git checkout master aep/aep_html.dyn_cfg
 
+### Uncomment to generate lib/${OPAL_VERSION}/cqls.js
+## cd lib;./opal2js;cd ..
+
 dyn -d github aep/aep_html.dyn
 
 cp aep/index.html .
 
 cp aep/aide/*.html aide
-
